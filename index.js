57769b7fe102b1fdde07f72687b1b81930cd3192
buisyngoc2811 <142831019+buisyngoc2811@users.noreply.github.com>
2026-04-21 17:10:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -816,15 +816,3 @@ client.on("guildMemberAdd", async (member) => {
 
 client.login(process.env.TOKEN);
 
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Bot is running");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Web server chạy");
-});
